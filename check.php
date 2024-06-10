455bcd52db63f0765ef449e6d923745262b48639
badibiboo <167818543+badibiboo@users.noreply.github.com>
2024-06-10 10:12:35 +0200

    Create check.php

---
+++ check.php
@@ -0,0 +1,4 @@
+// alle pagina's check -> session start en session end.
+// Check moet op alle pagina's die je wilt beschermen
+// 1 pagina, check.php en elke pagina verwijst naar check pagina checked of sessie is gestart,is het niet gestart dan redirect -> login.php 
+// if statement. 
